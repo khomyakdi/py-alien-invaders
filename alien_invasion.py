428041b31a8a419efa1c00e35b426272e97377fa
khomyakdi <khomyakdi@gmail.com>
2022-12-05 19:37:39 +0200

    Add game empty screen

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        self.bg_color = (230, 230, 230)
+        pygame.display.set_caption("Alien invasion")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
